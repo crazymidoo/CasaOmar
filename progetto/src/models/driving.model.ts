6da2bb94aecf44a964b1106ff6dd378f6d386a6c
ahmed <159021809+crazymidoo@users.noreply.github.com>
2025-05-23 13:13:39 +0000

    Punto 1

---
+++ progetto/src/models/driving.model.ts
@@ -0,0 +1,14 @@
+export class Driving{
+    tipo: string;
+    descrizione: string;
+    tariffa: string;
+    valutazionemedia: string
+
+    constructor(tipo:string, descrizione:string, tariffa:string, valutazionemedia:string){
+        this.tipo = tipo,
+        this.descrizione = descrizione,
+        this.tariffa = tariffa,
+        this.valutazionemedia = valutazionemedia
+    }
+
+}
